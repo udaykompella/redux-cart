e60bce0fa789dfc9920b921ad4e4e4f782c14fa6
udaykompella <kompellauday9@gmail.com>
2023-09-04 23:37:58 +0530

    changes corrected

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 
 import './App.css';
-import Product from './components/products';
+// import Product from './components/products';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { createBrowserRouter,createRoutesFromElements,RouterProvider } from 'react-router-dom';
+import {RootLayout} from './components/RootLayout'
+import  Dashboard from './components/Dashboard'
+import Cart from './components/Cart'
+import { createBrowserRouter,createRoutesFromElements,Route,RouterProvider } from 'react-router-dom';
 
 function App() {
   const router = createBrowserRouter(createRoutesFromElements(
-
+          <Route path='/' element={<RootLayout/>}>
+             <Route index element={<Dashboard/>} />
+             <Route path='/cart' element={<Cart />} />
+          </Route>
     ))
     return (
       <div className="App">
-         <Product />
+         <RouterProvider router={router}/>
       </div>
     );
 }
--- src/components/RootLayout.js
+++ src/components/RootLayout.js
@@ -1,9 +1,11 @@
 import {Outlet} from 'react-router-dom' 
 import NavBar from './NavBarPanel'
+import Product from './products'
 export const RootLayout = () => {
     return(
         <>
             <NavBar />
+            {/* <Product/> */}
 
             <main>
                 <Outlet />
